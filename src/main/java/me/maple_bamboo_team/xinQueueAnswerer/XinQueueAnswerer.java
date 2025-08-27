e2f483fda0a96ba66ee3f66c204ad3e0d7615ad1
Harvey <rhw15020306490@163.com>
2025-08-27 16:40:42 +0800

    commit

---
+++ src/main/java/me/maple_bamboo_team/xinQueueAnswerer/XinQueueAnswerer.java
@@ -0,0 +1,10 @@
+package me.maple_bamboo_team.xinQueueAnswerer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class XinQueueAnswerer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/maple_bamboo_team/xinQueueAnswerer/client/XinQueueAnswererClient.java
@@ -0,0 +1,10 @@
+package me.maple_bamboo_team.xinQueueAnswerer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XinQueueAnswererClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
